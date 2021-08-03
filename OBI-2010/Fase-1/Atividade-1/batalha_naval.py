9ee876e5e9d422b964c43dda507391f7a24dbcea
dyogobendo <dyogoromagnabendo@gmail.com>
2021-08-03 13:44:11 -0300

    menos pior

--- OBI-2010/Fase-1/Atividade-1/batalha_naval.py
+++ OBI-2010/Fase-1/Atividade-1/batalha_naval.py
@@ -1,60 +1,56 @@
 coordenadas_adicionadas = set()
 tabuleiro = []
+barcos = []    
 
-def adiciona_coordenada(barcos, existente, nova):
+def adiciona_coordenada(existente, nova):
     for barco in barcos:
         if existente in barco:
-            barco.append(nova)
+            barco.add(nova)
             coordenadas_adicionadas.add(nova)
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    barcos = []    
+    repeated = []
     for i in range(n):
         tabuleiro.append(input())
     for i in range(n):
         for j in range(m):
             if tabuleiro[i][j] == '#':
-                coordenadas = (i, j)                
-                if coordenadas not in coordenadas_adicionadas:
-                    if j < m - 1:
-                        vizinho_direita = (i, j + 1)
-                        if vizinho_direita in coordenadas_adicionadas:
-                            adiciona_coordenada(barcos, vizinho_direita, (i, j))
-                        else:
-                            barcos.append([(i,j)])
-                            coordenadas_adicionadas.add(coordenadas)    
-                    else:
-                        barcos.append([(i,j)])
-                        coordenadas_adicionadas.add(coordenadas)
-                if j < m - 1:
-                    if tabuleiro[i][j + 1] == '#':
-                        coordenadas = (i, j + 1)
-                        if coordenadas not in coordenadas_adicionadas:
-                            adiciona_coordenada(barcos, (i, j), coordenadas)
-                        else:
-                            for barco in barcos:
-                                if (i, j + 1) in barco:
-                                    barco.append((i, j))
-                                    coordenadas_adicionadas.add((i, j + 1))
-                if i < n - 1:
-                    if tabuleiro[i + 1][j] == '#':
-                        coordenadas = (i + 1, j)
-                        if coordenadas not in coordenadas_adicionadas:
-                            for barco in barcos:
-                                if (i, j) in barco:
-                                    barco.append(coordenadas)
-                                    coordenadas_adicionadas.add(coordenadas)
-                        else:
-                            for barco in barcos:
-                                if (i + 1, j) in barco:
-                                    barco.append((i, j))
-                                    coordenadas_adicionadas.add((i + 1, j))
+                add = 0
+                atual = (i, j)
+                esquerda = (i, j-1)
+                direita = (i, j + 1)
+                cima = (i - 1, j)
+                baixo = (i + 1, j)
+
+                if esquerda in coordenadas_adicionadas:
+                    adiciona_coordenada(esquerda, atual)
+                    add += 1
+                if direita in coordenadas_adicionadas:
+                    adiciona_coordenada(direita, atual)
+                    add += 1
+                if cima in coordenadas_adicionadas:
+                    adiciona_coordenada(cima, atual)
+                    add += 1
+                if baixo in coordenadas_adicionadas:
+                    adiciona_coordenada(baixo, atual)
+                    add += 1
+                if add == 0:
+                    barcos.append({atual})
+                elif add > 1:
+                    repeated.append(atual)      
+                coordenadas_adicionadas.add(atual)                
+    for coordenada in repeated:
+        merged = set()
+        for barco in barcos:
+            if coordenada in barco:
+                merged = merged.union(barco)
+                barcos.remove(barco)
+        barcos.append(merged)
+
     k = int(input())
 
-    barcos_destruidos = 0
-    print(barcos)
-    print()
+    barcos_destruidos = 0    
     for i in range(k):
         l, c = map(int, input().split())  
         for barco in barcos:
@@ -63,6 +59,5 @@ if __name__ == '__main__':
                 if len(barco) == 0:
                     barcos_destruidos += 1
             except:
-                pass    
-    print(barcos)
+                pass        
     print(barcos_destruidos)

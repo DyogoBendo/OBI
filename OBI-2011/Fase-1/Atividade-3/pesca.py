19b9f7be3df0643569ca85592133badecadb6b83
dyogobendo <dyogoromagnabendo@gmail.com>
2021-08-10 15:12:09 -0300

    sacanagem - considerar todos os pontos do mar

--- OBI-2011/Fase-1/Atividade-3/pesca.py
+++ OBI-2011/Fase-1/Atividade-3/pesca.py
@@ -1,57 +1,19 @@
 def main(file_input=input, test=False):
     n = int(file_input())    
-    squares = []
-    for i in range(n):
+    mar = [[False for _ in range(101)] for _ in range(101)]
+    for _ in range(n):
         xi, xf, yi, yf = map(int, file_input().split())
-        squares.append((xi, xf, yi, yf))
-    c = 0
 
-    for xi, xf, yi, yf in squares:
-        c += (xf - xi) * (yf - yi)  # calculo das areas totais
-
-    quadrados_resolvidos = []
-
-    for xi, xf, yi, yf in squares:
-        retangulos_intersec = []
-        for sxi, sxf, syi, syf in quadrados_resolvidos:
-            intersecta_x = False
-            intersecta_y = False                        
-
-            if xi < sxi:        
-                if sxi < xf:
-                    intersecta_x = True
-                    ixi = sxi
-                    ixf = xf                
-            elif xi > sxi:
-                if xi < sxf:
-                    intersecta_x = True
-                    ixi = xi
-                    ixf = sxf
-            else:
-                intersecta_x = True
-                ixi = xi
-                ixf = xf if xf < sxf else sxf
-            
-            if yi < syi:        
-                if syi < yf:
-                    intersecta_y = True
-                    iyi = syi
-                    iyf = yf                
-            elif yi > syi:
-                if yi < syf:
-                    intersecta_y = True
-                    iyi = yi
-                    iyf = syf
-            else:
-                intersecta_y = True
-                iyi = yi
-                iyf = yf if yf < syf else syf
-
-            if intersecta_x and intersecta_y:
-                rect_interc = (ixi, ixf, iyi, iyf)
-                retangulos_intersec.append(rect_interc)
+        for i in range(xi, xf):
+            for j in range(yi, yf):                
+                mar[i][j] = True
         
-        
+        c = 0
+        for i in range(101):
+            for j in range(101):
+                if mar[i][j]:
+                    c += 1
+  
 
     if not test:
         print(c)

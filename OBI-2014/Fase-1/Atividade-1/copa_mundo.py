2f60354249b4af3df2acadefc7a3a86130902591
dyogobendo <dyogoromagnabendo@gmail.com>
2021-08-17 08:49:11 -0300

    copa do mundo - arvore geradora minima

---
+++ OBI-2014/Fase-1/Atividade-1/copa_mundo.py
@@ -0,0 +1,40 @@
+# problema de arvore geradora minima
+from heapq import *
+
+def main(file_input=input, test=False):
+    n, f, r = map(int, file_input().split())
+
+    cidades = [[] for _ in range(n)]
+    ferrovias = [[] for _ in range(n)]
+    rodovias = [[] for _ in range(n)]
+    for i in range(f):
+        a, b, k = map(int, file_input().split())
+        a -= 1
+        b -= 1
+        cidades[a].append((0, k, b))
+        cidades[b].append((0, k, a))
+    for i in range(r):
+        a, b, k = map(int, file_input().split())
+        a -= 1
+        b -= 1
+        cidades[a].append((1, k, b))
+        cidades[b].append((1, k, a))
+
+    c = 0            
+    arvore_minima_vertices = {0}
+    arvore_minima_arestas = cidades[0]
+    heapify(arvore_minima_arestas)            
+    while arvore_minima_arestas:        
+        _, k, b = heappop(arvore_minima_arestas)    
+        if b not in arvore_minima_vertices:            
+            arvore_minima_vertices.add(b)
+            c += k
+            for t, w, u in cidades[b]:
+                heappush(arvore_minima_arestas, (t, w, u))    
+    if not test:
+        print(c)
+    else:
+        return [c]
+
+if __name__ == '__main__':
+    main()

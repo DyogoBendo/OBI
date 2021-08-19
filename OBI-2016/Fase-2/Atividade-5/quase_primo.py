93174e18cd77640e0079dda65691847412510f18
dyogobendo <dyogoromagnabendo@gmail.com>
2021-08-19 13:37:12 -0300

    melhor mas ainda com TLE

--- OBI-2016/Fase-2/Atividade-5/quase_primo.py
+++ OBI-2016/Fase-2/Atividade-5/quase_primo.py
@@ -1,11 +1,10 @@
+from math import sqrt
+
 def main(file_input=input, test=False):
     n, k = map(int, file_input().split())
     primos = list(map(int, file_input().split()))
-    numeros = [i for i in range(n + 1)]    
-    for i in primos:
-        for j in range(i, n + 1, i):            
-            numeros[j] = 0    
-    c = n - numeros.count(0) + 1
+    multiplos = {p * i for p in primos for i in range((n // p) + 1)}
+    c = n - len(multiplos) + 1
 
     if not test:
         print(c)

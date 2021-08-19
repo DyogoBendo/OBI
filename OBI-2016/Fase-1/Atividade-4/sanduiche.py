1f1aa36a1f312ed87b5e0e9715a8b88fdaad2db4
dyogobendo <dyogoromagnabendo@gmail.com>
2021-08-19 07:45:31 -0300

    sem ideias

---
+++ OBI-2016/Fase-1/Atividade-4/sanduiche.py
@@ -0,0 +1,12 @@
+def main(file_input=input, test=False):
+    n, d = map(int, file_input().split())
+    sanduiches = list(map(int, file_input().split()))
+    
+
+    if not test:
+        print(c)
+    else:
+        return [c]
+
+if __name__ == '__main__':
+    main()

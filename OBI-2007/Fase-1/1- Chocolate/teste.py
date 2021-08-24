1fa8b3cd3baed200d25429eadf87342d4af92997
dyogobendo <dyogoromagnabendo@gmail.com>
2021-08-24 19:06:51 -0300

    teste arquivos

--- OBI-2007/Fase-1/1- Chocolate/teste.py
+++ OBI-2007/Fase-1/1- Chocolate/teste.py
@@ -12,6 +12,13 @@ if __name__ == '__main__':
     print(current_dir)
     print(os.path.basename(current_dir))
     files =  Path(current_dir, "tests")    
-    create_test(main, files, number_tests=19, start_position=2, prefix="test")
+    txt = create_test(main, files, number_tests=19, start_position=2, prefix="test")
+
+    ARQUIVO_FINAL = "resultado_testes.md"
+
+    if os.path.exists(ARQUIVO_FINAL):
+        os.remove(ARQUIVO_FINAL)
+    f = open(ARQUIVO_FINAL, "w")
+    f.write(txt)
     
     
--- test_package/test_file.py
+++ test_package/test_file.py
@@ -4,16 +4,15 @@ import os
 PONTILHADO = "-"*100
 
 def create_test(main, files, number_tests=11, prefix="", sufix="", start_position=0):    
+    txt = ""
     passed_all_tests = True         
     num_conjunto_testes_certos = 0     
-    conjunto_testes_errados = set()  
-    print(PONTILHADO)   
+    conjunto_testes_errados = set()      
     for l in range(start_position, number_tests + start_position):        
         files_folder = Path(files,  prefix + str(l) + sufix)        
-        print(f"\t\033[95mCONJUNTO DE TESTES {l}: \033[0m")
-        print(PONTILHADO)
+        txt += f"\t\033[95mCONJUNTO DE TESTES {l}: \033[0m\n" 
+        txt += PONTILHADO + '\n'        
         passed_test_group = True
-
         num_testes_certos = 0
         num_testes = 0
         testes_errados = set()
@@ -30,21 +29,23 @@ def create_test(main, files, number_tests=11, prefix="", sufix="", start_positio
             passed_test = True            
 
             num_linhas_certas = 0
-            num_linhas = 0                                                
-            print(f"\t\tTESTE {i}: ")            
+            num_linhas = 0                                                            
+            txt += f"\t\tTESTE {i}: \n" 
             for j in range(len(result)):                
                 num_linhas += 1
                 gabarito = output_file.readline().strip()            
-                print(f"\t\t\tLINHA {j}: ")
-                print(f"\t\t\tGABARITO LINHA {j}:\t", gabarito.strip())
-                print(f"\t\t\tRESULTADO LINHA {j}:\t", result[j])
-                print()
+                txt += f"\t\t\tLINHA {j}: \n" 
+                txt += f"\t\t\tGABARITO LINHA {j}:\t" + gabarito.strip() 
+                txt += '\n'
+
+                txt += f"\t\t\tRESULTADO LINHA {j}:\t" + str(result[j])
+                txt += '\n'                
                 if gabarito == str(result[j]):
                     num_linhas_certas += 1
                 else:                    
                     passed_test = False                
 
-            if not passed_test:
+            if not passed_test:                
                 result_print = '\033[91m' + "ERRO" + '\033[0m'
                 testes_errados.add(str(i))
                 passed_test_group = False
@@ -52,9 +53,15 @@ def create_test(main, files, number_tests=11, prefix="", sufix="", start_positio
                 num_testes_certos += 1
                 result_print = '\033[92m' + "PASSOU" + '\033[0m'
 
-            print(f"\t\tRESULTADO TESTE {i}: " + result_print)
-            print(f"\t\tNÚMERO DE LINHAS CERTAS: {num_linhas_certas}/{num_linhas}")
-            print(PONTILHADO)                 
+            txt += f"\t\tRESULTADO TESTE {i}: " + result_print
+            txt += '\n'
+            
+            txt += f"\t\tNÚMERO DE LINHAS CERTAS: {num_linhas_certas}/{num_linhas}" 
+            txt += '\n'
+
+            txt += PONTILHADO
+            txt += '\n'
+
         if not passed_test_group:
             passed_all_tests = False
             result_print = '\033[91m' + "ERRO NOS TESTES: "
@@ -66,14 +73,24 @@ def create_test(main, files, number_tests=11, prefix="", sufix="", start_positio
             num_conjunto_testes_certos += 1
             result_print = '\033[92m' + "PASSOU" + '\033[0m'
 
-        print(f"\tRESULTADO GRUPO DE TESTES {l}: " + result_print)
-        print(f"\tNÚMERO DE TESTES CERTOS: {num_testes_certos}/{num_testes}")
-        print(PONTILHADO)
+        txt += f"\tRESULTADO GRUPO DE TESTES {l}: " + result_print
+        txt += '\n'
+
+        txt += f"\tNÚMERO DE TESTES CERTOS: {num_testes_certos}/{num_testes}"                 
+        txt += '\n'
+
+        txt += PONTILHADO        
+        txt += '\n'        
     conjunto_testes_errados
     result_print = '\033[92m' + "PASSOU" + '\033[0m' if passed_all_tests else '\033[91m' + "ERRO NOS CONJUNTOS DE TESTES: " + ", ".join(conjunto_testes_errados) +'\033[0m'
-    print("RESULTADO TOTAL: " + result_print)
-    print(f"NÚMERO DE CONJUNTOS DE TESTES CERTOS: {num_conjunto_testes_certos}/{number_tests}")
-    print(PONTILHADO)
 
+    txt += "RESULTADO TOTAL: " + result_print 
+    txt += '\n'        
     
+    txt += f"NÚMERO DE CONJUNTOS DE TESTES CERTOS: {num_conjunto_testes_certos}/{number_tests}" 
+    txt += '\n'        
     
+    txt += PONTILHADO 
+    txt += '\n'        
+
+    return txt    

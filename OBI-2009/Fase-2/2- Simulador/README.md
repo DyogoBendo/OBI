6820b26e0805253fe4af9c7d6908b50d1beff999
dyogobendo <dyogoromagnabendo@gmail.com>
2021-09-07 10:43:05 -0300

    não compreendi - simulador

--- OBI-2009/Fase-2/2- Simulador/README.md
+++ OBI-2009/Fase-2/2- Simulador/README.md
@@ -1,5 +1,6 @@
 [Formatação utilizada](https://katex.org/docs/supported.html)
 # Análise do Problema
-
+Eu não compreendi a solução de fato, então não consigo explicar
 
 # Análise do Algoritmo
+O(m^2)
--- OBI-2009/Fase-2/2- Simulador/simulador.py
+++ OBI-2009/Fase-2/2- Simulador/simulador.py
@@ -1,34 +1,37 @@
-def cut(a):
+def cut(a):  # realiza um corte naquele valor
     global n, interv
-    if a >= n or a<=0:
+    if a >= n or a<=0:  # corte é feito apenas se o valor estiver entre 1 e n - 1
         return
-    sum=0
+    sum=0  # soma das "distancias"
 
     id = 0
     for i in range(len(interv)):
-        qtd = abs(interv[i][0] - interv[i][1]) + 1
+        qtd = abs(interv[i][0] - interv[i][1]) + 1  
+        # pegamos a diferença do intervalo, o que seria a "distancia"
+        # por exemplo, no intervalo [7, 5] a distância é 3
 
-        if qtd + sum > a:
-            id = i
+        if qtd + sum > a:  # se adicionarmos essa distancia a soma, e ela for maior que "a"
+            id = i  # definimos o identificador como i, e saimos do loop
+            # estamos interessados apenas no primeiro valor em que a distância é maior que "a"
             break
         sum += qtd
 
-    a -= sum
-    if not a:
+    a -= sum  # "a" é subtraído pelas distâncias até chegar nele 
+    if not a:  # se a soma das distâncias for igual a "a", então não há nada que possamos fazer
         return
 
-    if interv[id][0] <= interv[id][1]:
-        mid = interv[id][0] + a
-        novo = [mid, interv[id][1]]
-        interv[id][1]  = mid -1
-    else:
-        mid = interv[id][0] - a
-        novo = [mid, interv[id][1]]
-        interv[id][1]=mid+1
+    if interv[id][0] <= interv[id][1]:  # se o primeiro valor do intervalo for menor ou igual ao segundo
+        mid = interv[id][0] + a  # o meio é o primeiro intervalo + a
+        novo = [mid, interv[id][1]]  # e criamos um novo intervalo, iniciando desse meio, e indo até o segundo intervalo
+        interv[id][1]  = mid -1  # e o intervalo anterior, tem seu segundo valor trocado para a metade - 1
+    else: # caso contrario
+        mid = interv[id][0] - a  # o meio é o inicial - a
+        novo = [mid, interv[id][1]] # o novo parte do meio e vai até o segundo
+        interv[id][1]=mid+1  # e o intervalo antigo agora vai até o meio + 1
     
-    interv.append(novo)
-    for i in range(len(interv) - 1, id + 1, -1):
-        interv[i], interv[i-1] = interv[i-1], interv[i]
+    interv.append(novo)  # adicionamos o novo intervalo criado à lista de intervalos
+    for i in range(len(interv) - 1, id + 1, -1): # do último até o depois de onde o intervalo alcançava a distância maxima
+        interv[i], interv[i-1] = interv[i-1], interv[i]  # invertemos 
 
 
 def soma (a, b):
@@ -37,9 +40,9 @@ def soma (a, b):
     resp= 0
 
     for i in range(len(interv)):
-        qtd = abs(interv[i][0] -interv[i][1]) + 1
-        if sum + qtd>= a and sum + qtd<= b:
-            resp += ((interv[i][0] +interv[i][1]) * qtd) // 2        
+        qtd = abs(interv[i][0] -interv[i][1]) + 1  # "distância"
+        if sum + qtd>= a and sum + qtd<= b:  # se a "distância" estiver contida no intervalo
+            resp += ((interv[i][0] +interv[i][1]) * qtd) // 2   # fazemos uma soma aritmética do intervalo 
         sum += qtd
 
     return resp
@@ -51,17 +54,17 @@ def inverte (a, b):
     fim = 0
 
     for i in range(len(interv)):
-        if sum == a-1:
-            ini = i
+        if sum == a-1:  # quando a "distancia" acumulada for igual ao inicio do intervalo - 1
+            ini = i  # definimos o inicio do intervalo
         qtd = abs(interv[i][0]-interv[i][1]) + 1
-        if sum + qtd == b:
-            fim = i
+        if sum + qtd == b:  # já se o intervalo for igual ao final do intervalo
+            fim = i  # definimos o seu fim
         sum += qtd
     
-    for i in range(ini, ((ini + fim) // 2) + 1):
-        interv[i], interv[fim-(i-ini)] = interv[fim-(i-ini)], interv[i]
-    for i in range(ini, fim + 1):        
-        interv[i][0], interv[i][1] = interv[i][1], interv[i][0]
+    for i in range(ini, ((ini + fim) // 2) + 1):  # do inicio até metade do intervalo
+        interv[i], interv[fim-(i-ini)] = interv[fim-(i-ini)], interv[i]  # invertemos os intervalos
+    for i in range(ini, fim + 1):        # agora do inicio até o fim
+        interv[i][0], interv[i][1] = interv[i][1], interv[i][0]  # invertemos onde começa e onde termina os intervalos
 
 def main(entrada=input, test=False):
     global n, interv
@@ -74,8 +77,8 @@ def main(entrada=input, test=False):
         a = int(a)
         b = int(b)
 
-        cut(a-1)
-        cut(b)
+        cut(a-1)  # realizamos o corte para o inicial
+        cut(b)  # e para o final
 
         if op == "S":            
             if test:
